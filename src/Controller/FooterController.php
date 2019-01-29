7635d17a8cdbe31e24eaec3d9ca2849078f879d8
HugoChambefort <hugo.chambefort@laposte.net>
2019-01-29 09:34:59 +0100

    Ajout politique confidentialité

---
+++ src/Controller/FooterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class FooterController extends AbstractController
+{
+
+
+    /**
+     * @Route ("/mentions_légales", name="mentions")
+     */
+    public function indexment()
+    {
+        return $this->render("/Footer/mention_legale.html.twig");
+    }
+
+
+    /**
+     * @Route ("/politique _confidentialite", name="politique")
+     */
+    public function indexpol()
+    {
+        return $this->render("/Footer/politique.html.twig");
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
